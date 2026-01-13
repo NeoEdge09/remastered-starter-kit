c3fd7cdcb7469e76e27c66e196c11c9c2d6774df
NeoEdge09 <dikapp23@gmail.com>
2026-01-13 10:25:55 +0700

    Remove Frontend Route

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,7 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'active'])->name('dashboard');
 
-Route::middleware(['auth', 'active', 'permission.auto'])->name('frontend.')->group(function () {
-    // Users Management
-    Route::resource('users', UserController::class);
 
-});
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
